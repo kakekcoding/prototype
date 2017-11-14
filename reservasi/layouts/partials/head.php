d3ac0fdf1d0a8170665c62c9bafde5c89d97f309
Kakek Coding <32747943+kakekcoding@users.noreply.github.com>
2017-11-14 22:01:52 +0700

    Head fixed.

--- reservasi/layouts/partials/head.php
+++ reservasi/layouts/partials/head.php
@@ -8,5 +8,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 
+<link rel="stylesheet" href="../static/css/bootstrap.css" />
 <link rel="stylesheet" href="../static/css/bootstrap.min.css" />
 <link rel="stylesheet" href="../static/css/utama/style.css" />
