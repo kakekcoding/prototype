6d6a7f9ba6724d9584c1285d79039f414f95d50b
Kakek Coding <32747943+kakekcoding@users.noreply.github.com>
2017-11-14 21:48:06 +0700

    Navbar fixed!

--- reservasi/layouts/partials/navbar.php
+++ reservasi/layouts/partials/navbar.php
@@ -1,10 +1,11 @@
-<?php  
-	//hightlight menu aktif
-	// $namaHalaman = $_SERVER['PHP_SELF']
-	// $namaArray   = explode('/../',$namaHalaman);
-	// $jumlah      = count($namaArray);
-	// $namaHalaman = $namaArray[$jumlah-1];
+<?php
+	//Buat hightlight menu
+	$namaHalaman = $_SERVER['PHP_SELF'];
+	$namaArray   = explode('/',$namaHalaman);
+	$jumlah      = count($namaArray);
+	$namaHalaman = $namaArray[$jumlah-1];
 ?>
+
 
 <nav class="navbar navbar-default navbar-fixed-top navbar-ku" role="navigation" style="background-color: #314559; border-top-color: #6DB532; border-style: stripped; border-top-width: medium;">
 	<div class="container">
@@ -18,11 +19,11 @@
 		</div>	
 		<div class="collapse navbar-collapse" id="collapse">	
 			<ul class="nav navbar-nav navbar-right primary-menu">
-				<li class="<?php echo ($namaHalaman=='index.php')?'active':'';?>" >
-					<a class="<?php echo ($namaHalaman=='index.php')?'active':'';?>" href="index.php" >Beranda</a>
+				<li class="<?php echo ($namaHalaman=='#')?'active':'';?>" >
+					<a class="<?php echo ($namaHalaman=='#')?'active':'';?>" href="#" >Beranda</a>
 				</li>
-				<li class="<?php echo ($namaHalaman=='/../index.php')?'active':'';?>" >
-					<a class="<?php echo ($namaHalaman=='reservasi.php')?'active':'';?>" href="reservasi.php" >Reservasi</a>
+				<li class="<?php echo ($namaHalaman=='index.php')?'active':'';?>" >
+					<a class="<?php echo ($namaHalaman=='index.php')?'active':'';?>" href="index.php" >Reservasi</a>
 				</li>
 				<li class="<?php echo ($namaHalaman=='paket.php')?'active':'';?>" >
 					<a class="<?php echo ($namaHalaman=='paket.php')?'active':'';?>" href="paket.php" >Paket</a>
@@ -37,3 +38,4 @@
 		</div>
 	</div>
 </nav>
+<!--Akhir navbar-->
