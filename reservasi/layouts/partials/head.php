072bf9940ccc9410d8a087be727b337d6bdef60e
Kakek Coding <32747943+kakekcoding@users.noreply.github.com>
2017-11-14 20:14:27 +0700

    Error fixed!
    Error fixed!

--- reservasi/layouts/partials/head.php
+++ reservasi/layouts/partials/head.php
@@ -8,5 +8,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
 
-<link rel="stylesheet" href="../../static/css/bootstrap.min.css" />
-<link rel="stylesheet" href="../../static/css/utama/style.css" />
+<link rel="stylesheet" href="../static/css/bootstrap.min.css" />
+<link rel="stylesheet" href="../static/css/utama/style.css" />
