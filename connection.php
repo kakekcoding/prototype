36049b16b59cb55f53e918c43d17a12dd09488ff
Kakek Coding <32747943+kakekcoding@users.noreply.github.com>
2017-12-08 17:53:46 +0700

    adding

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+	class Connection{
+    function getConnection(){
+        $host       = "localhost";
+        $username   = "root";
+        $password   = "";
+        $dbname     = "db_tripon";
+	        try{
+	            $conn    = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+	            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	            return $conn;
+        	}catch (PDOException $e){
+            echo "ERROR CONNECTIONF : " . $e->getMessage();
+        	}
+    	}
+	}
+?>
